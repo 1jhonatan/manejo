46dd84c4aae57679099fe2fd3dfd58090e95e186
1jhonatan <114009119+1jhonatan@users.noreply.github.com>
2022-09-20 11:24:45 -0500

    Update copiar.bat

--- copiar.bat
+++ copiar.bat
@@ -1,4 +1,4 @@
 @echo off
-xcopy D:\user\ISER2022\Desktop\archivo D:\backup\Archivo-%date%
-set fileName=db_%date:~-4,4%%date:~-10,2%%date:~-7,2%_%time:~0,2%%time:~3,2%.bak
+xcopy D:\user\ISER2022\Desktop\archivo D:\backup\Archivo-%date:~-4,4%%date:~-10,2%%date:~-7,2%
+
 pause
